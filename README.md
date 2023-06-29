1b4ac0db52ce2e3af460f56f2e3f2f7d6def5bde
FlorCollosso <florcollosso@gmail.com>
2023-06-29 02:52:42 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# calculadoraInteresReinvertidos
+ Calculadora que permite conocer cuánto capital se puede acumular a lo largo de un período determinado al reinvertir una parte de los intereses mensuales.
---
+++ script.js
@@ -0,0 +1,68 @@
+// function calcularInteres() {
+//   const cifraInicial = parseFloat(document.getElementById('cifraInicial').value);
+//   const tasaInteresAnual = parseFloat(document.getElementById('tasaInteres').value);
+//   const tasaReinversionMensual = parseFloat(document.getElementById('tasaReinversion').value);
+//   const periodoMeses = parseInt(document.getElementById('periodoMeses').value);
+
+//   const tasaInteresDecimal = tasaInteresAnual / 100;
+//   const tasaReinversionDecimal = tasaReinversionMensual / 100;
+
+//   let totalInteresAcumulado = 0;
+//   let cifraActual = cifraInicial;
+
+//   for (let mes = 1; mes <= periodoMeses; mes++) {
+//     const interesMensual = cifraActual * (tasaInteresDecimal / 12);
+//     const reinversionMensual = interesMensual * tasaReinversionDecimal;
+
+//     totalInteresAcumulado += interesMensual + reinversionMensual;
+//     cifraActual += interesMensual + reinversionMensual;
+//   }
+
+//   document.getElementById('resultado').innerHTML = `El total de interés acumulado es: $${totalInteresAcumulado.toFixed(2)}`;
+// }
+
+// Función para calcular el interés acumulado
+function calcularInteresAcumulado() {
+  const cifraInicial = parseFloat(document.getElementById('cifraInicial').value);
+  const tasaInteresAnual = parseFloat(document.getElementById('tasaInteres').value);
+  const tasaReinversionMensual = parseFloat(document.getElementById('tasaReinversion').value);
+  const periodoMeses = parseInt(document.getElementById('periodoMeses').value);
+
+  const tasaInteresDecimal = tasaInteresAnual / 100;
+  const tasaReinversionDecimal = tasaReinversionMensual / 100;
+
+  let cifraActual = cifraInicial;
+  let interes = 0;
+
+  for (let mes = 1; mes <= periodoMeses; mes++) {
+    const interesMensual = cifraActual * (tasaInteresDecimal / 365) * 30;
+    const reinversionMensual = interesMensual * tasaReinversionDecimal;
+    cifraActual += reinversionMensual;
+    interes += reinversionMensual;
+  } 
+
+  document.getElementById('resultado').innerHTML = cifraActual.toFixed(2);
+  document.getElementById('interes').innerHTML = interes.toFixed(2);
+}
+
+// Funcion cursor
+
+document.addEventListener('mousemove', function(e) {
+  var customCursor = document.querySelector('.cursor');
+  customCursor.style.left = e.clientX + 'px';
+  customCursor.style.top = e.clientY + 'px';
+});
+
+var elements = document.querySelectorAll('calculator'); // Elementos a mostrar al hacer hover
+
+elements.forEach(function(element) {
+  element.addEventListener('mouseenter', function() {
+    var customCursor = document.querySelector('.custom-cursor');
+    customCursor.classList.add('hover');
+  });
+
+  element.addEventListener('mouseleave', function() {
+    var customCursor = document.querySelector('.custom-cursor');
+    customCursor.classList.remove('hover');
+  });
+});
