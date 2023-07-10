f9b90e410b275d6734be2f0a5f5a6ced9fc6a807
FlorCollosso <florcollosso@gmail.com>
2023-07-10 01:44:52 -0300

    Feature empty inputs, fix styles

--- script.js
+++ script.js
@@ -7,12 +7,12 @@ function calcularInteresAcumulado() {
   const resultado = document.getElementById('resultado');
   const interesAcu = document.getElementById('interes');
   const mesAMes = document.getElementById('mesAMes');
-  const infoText = document.getElementById('infoText');
+  const datosTable = document.getElementById('datosTable');
 
   const tasaInteresDecimal = tasaInteresAnual / 100;
   const tasaReinversionDecimal = tasaReinversionMensual / 100;
 
-  const opciones = { 
+  const opciones = {
     useGrouping: true,
     minimumFractionDigits: 0,
     maximumFractionDigits: 0,
@@ -20,12 +20,33 @@ function calcularInteresAcumulado() {
     style: "decimal",
   };
 
+  var inputs = document.getElementsByTagName('input');
+
+  // Verificar si alguno de los inputs está vacío
+  var isEmpty = false;
+  for (var i = 0; i < inputs.length; i++) {
+    if (inputs[i].value === '') {
+      isEmpty = true;
+      const errorMessageIn = 'Este campo es obligatorio.';
+      showError(inputs[i], errorMessageIn);
+    };
+  };
+
+  // Si hay campos vacíos, no se envía el formulario
+  if (isEmpty) {
+    return;
+  };
+
+  // Si los campos no están vacíos, añadir función toggleOn al elemento mes a mes
+  mesAMes.onclick = toggleOn;
+
   let cifraActual = cifraInicial;
   let interes = 0;
   let interesUltimoMes = 0;
   let mesNum = 1;
 
-  infoText.innerHTML = '';
+  //Limpiar Tabla
+  datosTable.innerHTML = '';
 
   for (let mes = 1; mes <= periodoMeses; mes++) {
     const interesMensual = cifraActual * (tasaInteresDecimal / 365) * 30;
@@ -34,49 +55,64 @@ function calcularInteresAcumulado() {
     interes += reinversionMensual;
     interesUltimoMes = reinversionMensual;
 
-    titleH3 = document.createElement('h3');
-    titleH3.textContent = `Mes ${mesNum}`;
-    infoText.appendChild(titleH3);
+    const row = document.createElement('tr');
+
+    // Crear elemento <td> para mostrar el número de mes y añadir a row
+    const title = document.createElement('td');
+    title.textContent = `${mesNum}`;
+    row.appendChild(title);
     mesNum += 1;
 
-    parrafo = document.createElement('p');
-    parrafo.textContent = `$ ${interesUltimoMes.toLocaleString('es', opciones)}`;
-    infoText.appendChild(parrafo);
-  } 
+    // Crear elemento <td> para mostrar el interés acumulado del mes y añadir a row
+    const cifraInteres = document.createElement('td');
+    cifraInteres.textContent = `$ ${interesUltimoMes.toLocaleString('es', opciones)}`;
+    row.appendChild(cifraInteres);
+
+    // Crear elemento <td> para mostrar el capital retirado y añadir a row
+    const capRetirado = document.createElement('td');
+    capRetirado.textContent = `$ ${(interesUltimoMes * tasaReinversionMensual / 100).toLocaleString('es', opciones)}`;
+    row.appendChild(capRetirado);
+
+    // Añadir row a datosTable
+    datosTable.appendChild(row);
+  };
 
   resultado.innerHTML = `$ ${cifraActual.toLocaleString('es', opciones)}`;
   interesAcu.innerHTML = `$ ${interes.toLocaleString('es', opciones)}`;
   mesAMes.innerHTML = 'Ver';
-}
+};
 
+// Función para abrir ventana de detalles
 function toggleOn() {
   const panelMesAMes = document.getElementById('containerInfo');
   panelMesAMes.style.display = 'flex';
-}
+};
 
+// Función para cerrar ventana de detalles
 function toggleOff() {
   const panelMesAMes = document.getElementById('containerInfo');
   panelMesAMes.style.display = 'none';
+};
+
+// Función para limpiar el mensaje de error y color rojo al cambiar el valor del input
+function clearError(input) {
+  input.classList.remove('error-input');
+  input.placeholder = '';
 }
 
-let calculado = false;
-const btn = document.getElementById('btn');
-
-btn.addEventListener('click', () => {
-  calculado = true;
-  mesAMes.onclick = toggleOn;
-});
+// Función para mostrar mensaje de error y cambiar el color del input
+function showError(input, errorMessage) {
+  input.classList.add('error-input');
+  input.placeholder = errorMessage;
+}
 
 // Función cursor
-
 var isTouchDevice = 'ontouchstart' in window || navigator.maxTouchPoints > 0 || navigator.msMaxTouchPoints > 0;
 
-if (!isTouchDevice) {  
-  document.addEventListener('mousemove', function(e) {
+if (!isTouchDevice) {
+  document.addEventListener('mousemove', function (e) {
     var customCursor = document.querySelector('.cursor');
     customCursor.style.left = e.clientX + 'px';
     customCursor.style.top = e.clientY + 'px';
   });
-
-  document.getElementsByTagName('*').style.cursor = 'none';
-}
+};
